ca88d025a297bf53f42d6314dab9319ff637b651
LaQuira Logan <laquira.logan@g.austincc.edu>
2025-12-09 03:42:10 +0000

    List Multiplication (5x10) table

--- tests/question_tests/question_tests.py
+++ tests/question_tests/question_tests.py
@@ -2,11 +2,31 @@
 import unittest
 
 #follow this example to add questions b, c, and d for testing including their functions
-from src.question_a.question_a import test_config
+from src.question_a.question_a import get_multiplication_table
+
 
 class Test_Config(unittest.TestCase):
 
     def test_question_a_config(self):
         self.assertEqual(True, test_config())
+
+class Test_Multiplication_Table(unittest.TestCase): #test class for multiplication table functions
+
+    def test_get_multiplication_table(self):
+        table = get_multiplication_table(5, 10)
+        expected_table = [
+            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+            [2, 4, 6, 8, 10, 12, 14, 16, 18, 20],
+            [3, 6, 9, 12, 15, 18, 21, 24, 27, 30],
+            [4, 8, 12, 16, 20, 24, 28, 32, 36, 40],
+            [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
+        ]   
+
+        self.assertEqual(table[0], [1,2,3,4,5,6,7,8,9,10]) #check first row
+        self.assertEqual(table[4], [5,10,15,20,25,30,35,40,45,50])#check last row
+        self.assertEqual(table[2][3], 12) #check specific value in table at index [2][3] ( 3rd row, 4th column)
+
+def test_config():
+    return True
 
 
